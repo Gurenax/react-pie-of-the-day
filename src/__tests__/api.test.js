51e0af233664efaf4ba73131dac3186786586aa2
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-23 20:32:51 +1100

    Add tests for api connectivity

---
+++ src/__tests__/api.test.js
@@ -0,0 +1,77 @@
+import api from '../api/init'
+
+describe('Check the connectivty when endpoints are valid', () => {
+  test('All Stores: It should respond as status OK', async () => {
+    const given = '/stores'
+    const expected = 200
+    let response = null
+    try {
+      response = await api.get(given)
+    } catch(error) {
+      response = error.response
+    }
+    expect(response.status).toBe(expected)
+  })
+
+  test('Store Pies: It should respond as status OK', async () => {
+    const given = '/stores/1/pies'
+    const expected = 200
+    let response = null
+    try {
+      response = await api.get(given)
+    } catch(error) {
+      response = error.response
+    }
+    expect(response.status).toBe(expected)
+  })
+
+  test('All Pies: It should respond as status OK', async () => {
+    const given = '/pies'
+    const expected = 200
+    let response = null
+    try {
+      response = await api.get(given)
+    } catch(error) {
+      response = error.response
+    }
+    expect(response.status).toBe(expected)
+  })
+})
+
+describe('Check the connectivty when endpoints are invalid', () => {
+  test('All Stores: It should respond as status 404', async () => {
+    const given = '/stores2'
+    const expected = 404
+    let response = null
+    try {
+      response = await api.get(given)
+    } catch(error) {
+      response = error.response
+    }
+    expect(response.status).toBe(expected)
+  })
+
+  test('Store Pies: It should respond as status 404', async () => {
+    const given = '/stores/1/pies2'
+    const expected = 404
+    let response = null
+    try {
+      response = await api.get(given)
+    } catch(error) {
+      response = error.response
+    }
+    expect(response.status).toBe(expected)
+  })
+
+  test('All Pies: It should respond as status 404', async () => {
+    const given = '/pies2'
+    const expected = 404
+    let response = null
+    try {
+      response = await api.get(given)
+    } catch(error) {
+      response = error.response
+    }
+    expect(response.status).toBe(expected)
+  })
+})
