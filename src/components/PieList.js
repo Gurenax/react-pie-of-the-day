8cc9dcca7f2007297a08ae8d726288c3c76bde90
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-24 09:44:56 +1100

    Remove top margin from className

--- src/components/PieList.js
+++ src/components/PieList.js
@@ -5,7 +5,7 @@ import Filters from './Filters'
 const PieList = ({ pies, stores, onSearch, onSort }) => (
   <div>
     <Filters onSearch={onSearch} onSort={onSort} />
-    <div className="filter-form-group d-flex flex-wrap flex-column flex-md-row mt-4 pie-list mx-auto justify-content-center justify-content-md-start">
+    <div className="filter-form-group d-flex flex-wrap flex-column flex-md-row pie-list mx-auto justify-content-center justify-content-md-start">
       {pies.map((pie, index) => (
         <PieItem key={'PieIndex' + index} pie={pie} stores={stores} />
       ))}
