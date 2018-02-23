47ccd60d121e1cc173acca515453ab84fbbb5c7e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-23 19:50:04 +1100

    Add main container-fluid div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import './App.css'
 import './bootstrap-4/css/bootstrap.min.css'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Pie of the Day</h1>
+        <div className="container-fluid">
+          <h1>Pie of the Day</h1>
+        </div>
       </div>
     )
   }
