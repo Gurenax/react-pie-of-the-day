8781d62d6de03fab83165bad27a026687e30e330
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-24 01:27:40 +1100

    Fix code formatting

--- src/components/PieItem.js
+++ src/components/PieItem.js
@@ -7,29 +7,38 @@ const PieItem = ({ pie, stores }) => {
   let styleRating = {}
   if (store.rating > 7) {
     styleRating = { ...styles.rating, ...styles.ratingHigh }
-  }
-  else if (store.rating > 3) {
+  } else if (store.rating > 3) {
     styleRating = { ...styles.rating, ...styles.ratingMid }
-  }
-  else {
+  } else {
     styleRating = { ...styles.rating, ...styles.ratingLow }
   }
 
   return (
-    <div className="card mr-2 mb-2 pie-card">
-      <div className="position-absolute text-center raleway" style={styleRating}>
+    <div className="card mr-4 mb-4 pie-card">
+      <div
+        className="position-absolute text-center raleway"
+        style={styleRating}
+      >
         {store.rating}
       </div>
 
       <div className="card-body">
-        <h2 className="card-title pie-card-title raleway">{pie.displayName}</h2>
-        <h6 className="card-subtitle raleway mb-2 text-muted">{store.displayName}</h6>
+        <h2 className="card-title pie-card-title font-weight-bold raleway">
+          {pie.displayName}
+        </h2>
+        <h6 className="card-subtitle raleway mb-2 text-muted">
+          {store.displayName}
+        </h6>
         <p className="card-text raleway">
-          {pie.priceString} ({getQuantityDescription(pie.quantity)})
+          <span className="pie-card-price">{pie.priceString}</span>{' '}
+          <span className="pie-card-description">
+            ({getQuantityDescription(pie.quantity)})
+          </span>
         </p>
-        <p className="card-text raleway">
-          {getStoreFullAddress(store)}<br/>
-          { !!store.mobile && 'Contact No: '+ store.mobile}
+        <p className="card-text raleway pie-card-description">
+          {getStoreFullAddress(store)}
+          <br />
+          {!!store.mobile && 'Contact No: ' + store.mobile}
         </p>
       </div>
     </div>
