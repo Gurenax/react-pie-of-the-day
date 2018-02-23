7469d28da24f10c2b01d62e16c70cefaac23fecd
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-24 09:08:13 +1100

    Add tests for search and filters

--- src/__tests__/pies.test.js
+++ src/__tests__/pies.test.js
@@ -1,4 +1,12 @@
-import { listPies, listPiesOfTheDay, getQuantityDescription } from '../api/pies'
+import {
+  listPies,
+  listPiesOfTheDay,
+  getQuantityDescription,
+  sortPiesByPriceAscending,
+  sortPiesByPriceDescending,
+  sortPies,
+  searchPies
+} from '../api/pies'
 
 describe('When retrieving a list of pies', () => {
   test('It should fetch a list of pies', async () => {
@@ -74,3 +82,112 @@ describe('When translating the quantity to its appropriate description', () => {
     expect(result).toEqual(expected)
   })
 })
+
+describe('When sorting pies by price', () => {
+  let data = []
+  beforeAll(() => {
+    data = [
+      {
+        id: 1,
+        storeId: 1,
+        displayName: 'Beef',
+        quantity: 5,
+        price: 300,
+        priceString: '$3.00',
+        isPieOfTheDay: true
+      },
+      {
+        id: 1,
+        storeId: 1,
+        displayName: 'Beef and curry',
+        quantity: 2,
+        price: 250,
+        priceString: '$2.50'
+      },
+      {
+        id: 1,
+        storeId: 1,
+        displayName: 'Chicken',
+        quantity: 0,
+        price: 270,
+        priceString: '$2.70'
+      }
+    ]
+  })
+
+  test('It should sort the pies in ascending order', () => {
+    const expected = data.sort((a, b) => {
+      return a.price - b.price
+    })
+    const result = sortPiesByPriceAscending(data)
+    expect(result).toEqual(expected)
+  })
+
+  test('It should sort the pies in ascending order when function used is for descending', () => {
+    const expected = data.sort((a, b) => {
+      return a.price - b.price
+    })
+    const result = sortPiesByPriceDescending(data)
+    expect(result).not.toEqual(expected)
+  })
+
+  test('It should sort the pies in descending order', () => {
+    const expected = data.sort((a, b) => {
+      return b.price - a.price
+    })
+    const result = sortPiesByPriceDescending(data)
+    expect(result).toEqual(expected)
+  })
+
+  test('It should sort the pies in descending order when function used is for ascending', () => {
+    const expected = data.sort((a, b) => {
+      return b.price - a.price
+    })
+    const result = sortPiesByPriceAscending(data)
+    expect(result).not.toEqual(expected)
+  })
+
+  test('It should sort the pies in ascending order when using a sortKey', () => {
+    const given = 'priceAsc'
+    const expected = data.sort((a, b) => {
+      return a.price - b.price
+    })
+    const result = sortPies(data, given)
+    expect(result).toEqual(expected)
+  })
+
+  test('It should not sort the pies in ascending order when using the wrong sortKey', () => {
+    const given = 'priceDesc'
+    const expected = data.sort((a, b) => {
+      return a.price - b.price
+    })
+    const result = sortPies(data, given)
+    expect(result).not.toEqual(expected)
+  })
+
+  test('It should sort the pies in descending order when using a sortKey', () => {
+    const given = 'priceDesc'
+    const expected = data.sort((a, b) => {
+      return b.price - a.price
+    })
+    const result = sortPies(data, given)
+    expect(result).toEqual(expected)
+  })
+
+  test('It should not sort the pies in descending order when using the wrong sortKey', () => {
+    const given = 'priceAsc'
+    const expected = data.sort((a, b) => {
+      return b.price - a.price
+    })
+    const result = sortPies(data, given)
+    expect(result).not.toEqual(expected)
+  })
+})
+
+
+// // Search Pies
+// export const searchPies = (pies, searchKey) => {
+//   return pies.filter(pie => {
+//     return pie.displayName.toLowerCase().match(searchKey.toLowerCase())
+//   })
+// }
